6e9ba7f6179df4d38a01127e9e2d773e91e79f08
Wouter Bolsterlee <uws@xs4all.nl>
2014-10-14 11:57:55 +0200

    Initial setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='temporenc',
+    version='0.1',
+    author="Wouter Bolsterlee",
+    author_email="uws@xs4all.nl",
+    packages=find_packages(),
+)

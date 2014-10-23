c341259964b4874656f0394d459fe46b5e7d010e
Wouter Bolsterlee <uws@xs4all.nl>
2014-10-23 17:31:24 +0200

    Add __version__ and __version_info__ package attributes
    See #3.

--- temporenc/__init__.py
+++ temporenc/__init__.py
@@ -2,8 +2,11 @@
 Temporenc, a comprehensive binary encoding format for dates and times
 """
 
-# Export public API
+__version__ = '0.1.0'
+__version_info__ = tuple(map(int, __version__.split('.')))
 
+
+# Export public API
 from .temporenc import (  # noqa
     pack,
     packb,

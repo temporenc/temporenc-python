1108674efd26e3e629e3e8eafd5dc15853df53cb
Wouter Bolsterlee <uws@xs4all.nl>
2014-10-23 17:33:37 +0200

    Get version info from package in setup script
    See #3.

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+# No third party dependencies, so importing the package should be safe.
+import temporenc
+
 setup(
     name='temporenc',
-    version='0.1',
+    version=temporenc.__version__,
     author="Wouter Bolsterlee",
     author_email="uws@xs4all.nl",
     url='https://github.com/wbolster/temporenc-python',
